54b3e067c60ce17cd9339cead499a5b1e658f5e2
SxmmyLe <samleworthysound@gmail.com>
2026-08-16 22:10:01 +0100

    Add launcher service worker

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE='job-knock-launcher-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-1024.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(fetch(req).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  if(new URL(req.url).origin===self.location.origin){
+    event.respondWith(caches.match(req).then(hit=>hit||fetch(req)));
+  }
+});
